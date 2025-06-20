6f5ffc11ed5d9b28e90f5635b893f900b7b4ee8f
VadimsMalickis <vadims.malickis@rvt.lv>
2025-06-20 20:34:17 +0300

    write to file

--- tsi-practice-cpp/main.cpp
+++ tsi-practice-cpp/main.cpp
@@ -1,37 +1,24 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-	char* str1 = new char[10];
-	char* str2 = new char[10];
-	cin >> str1;
-	cin >> str2;
-	int len1 = 0;
-	int len2 = 0;
-
-	while (str1[len1] != '\0') {
-		++len1;
-	}
-
-	while (str1[len2] != '\0') {
-		++len2;
-	}
- 	char* result = new char[len1 + len2 + 1];
-
-	// Copy str1 to result
-	int i = 0;
-	for (; i < len1; ++i) {
-		result[i] = str1[i];
-	}
-	// Copy str2 to result
-	for (int j = 0; j < len2; ++j, ++i) {
-		result[i] = str2[j];
-	}
-
-	// Null-terminate the result
-	result[i] = '\0';
-
-	cout << result;
+	string str;
+	cout << "Enter csv row: ";
+	getline(cin, str);
+	FILE* file;
+	fopen_s(&file, "data.csv", "a");
+	if (file == NULL)
+		printf("File open error\n");
+	else
+	{
+		fprintf(file, "%s\n", str.c_str());
+		printf("File successfully opened\n");
+		fclose(file);
+		printf("File successfully closed\n");
 	
+	}
+
+	return 0;
 }
